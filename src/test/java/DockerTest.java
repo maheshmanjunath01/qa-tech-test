6f92ef1751639a9a6dcaf8cf2aed09e551ab2ec1
Mahesh Manjunath <mahesh2live@gmail.com>
2021-05-06 01:12:36 +0100

    formatted files

--- src/test/java/DockerTest.java
+++ src/test/java/DockerTest.java
@@ -1,17 +1,13 @@
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 import pageObjects.HomePageObjects;
 import utils.DriverFactory;
-import utils.GenericFunctions;
 
 public class DockerTest extends DriverFactory {
-    //  GenericFunctions genericFunctions = PageFactory.initElements(driver, GenericFunctions.class);
     HomePageObjects homePage = PageFactory.initElements(driver, HomePageObjects.class);
     private static final Logger log = LogManager.getLogger(DockerTest.class.getName());
 
